afbf4eb93b9d1197a21150b2e751dbc6fed34913
SangeethaGnanavel <sangeetha.gnana@gmail.com>
2023-12-17 16:02:27 -0500

    sql queires

---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO department (name)
+VALUES ("Research"),
+       ("Human Resource"),
+       ("Marketing"),
+       ("Training"),
+       ("Executive");
+
+INSERT INTO role (title,salary,department_id)
+VALUES("Junior Research Engineer",1000,1),
+      ("Senior Research Engineer",2000,1),
+      ("Junior Human Resource",1500,2),
+      ("Senior Human Resource",2000,2),
+      ("Junior Marketing Executive",2500,3),
+      ("Senior Marketing Executive",3000,3),
+      ("Trainee",800,4),
+      ("Trainer",2000,4),
+      ("COO",5000,5),
+      ("CEO",5000,5);
+
+
+
